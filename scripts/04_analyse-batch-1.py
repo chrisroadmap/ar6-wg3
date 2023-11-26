d68bc83017bef0305ed92256833c08d57d51802e
Chris Smith <chrisroadmap@gmail.com>
2023-11-26 19:43:47 +0000

    this data won't regress without a fight

--- scripts/04_analyse-batch-1.py
+++ scripts/04_analyse-batch-1.py
@@ -14,9 +14,18 @@ from tqdm.auto import tqdm
 load_dotenv()
 datadir = os.getenv("DATADIR")
 batch_size = int(os.getenv("BATCH_SIZE"))
+here = os.path.dirname(os.path.realpath(__file__))
 
 ar6_wg3 = pd.read_csv(os.path.join(datadir, "emissions", "20220314_ar6emissions_harmonized_infilled.csv"))
 vetted = pd.read_csv(os.path.join(datadir, "emissions", "passing-vetting.csv"))
+params = pd.read_csv(os.path.join(here, "..", "calibrations", "v1.3.0", "calibrated_constrained_parameters.csv"), index_col=0)
+
+params.aci_beta = np.log(-params.aci_beta)
+params.aci_shape_so2 = np.log(params.aci_shape_so2)
+params.aci_shape_bc = np.log(params.aci_shape_bc)
+params.aci_shape_oc = np.log(params.aci_shape_oc)
+
+params.drop(columns=['seed'], inplace=True)
 
 # get CO2 FFI emissions as the predictor
 batch_start = 0
@@ -36,74 +45,66 @@ for iscen in range(batch_start, batch_end):
         (filt["Variable"]=="AR6 climate diagnostics|Infilled|Emissions|CO2|Energy and Industrial Processes"),
         "2015":"2100"
     ].values.squeeze() * 0.001
-    so2_emissions[:, iscen] = filt.loc[
-        (filt["Variable"]=="AR6 climate diagnostics|Infilled|Emissions|Sulfur"),
-        "2015":"2100"
-    ].values.squeeze()
-
-pl.scatter(co2_emissions.ravel(), so2_emissions.ravel())
-pl.show()
-
-# print(co2_emissions)
-# pl.plot(co2_emissions)
+#     so2_emissions[:, iscen] = filt.loc[
+#         (filt["Variable"]=="AR6 climate diagnostics|Infilled|Emissions|Sulfur"),
+#         "2015":"2100"
+#     ].values.squeeze()
+#
+# pl.scatter(co2_emissions.ravel(), so2_emissions.ravel())
 # pl.show()
 
-# pl.scatter(f.emissions.loc[dict(specie='CO2', scenario='ssp119')], fnon[:, 0, :])
-# pl.scatter(f.emissions.loc[dict(specie='CO2', scenario='ssp245')], fnon[:, 1, :])
-# pl.scatter(f.emissions.loc[dict(specie='CO2', scenario='ssp585')], fnon[:, 2, :])
-# pl.show()
 
 ds = xr.load_dataset(os.path.join(datadir, "output", f"batch_0001.nc"))
-print(ds)
-    #
-    # ds = xr.Dataset(
-    #     data_vars=dict(
-    #         temperature=(["timepoints", "scenario", "config"], temp),
-    #         concentration=(["timepoints", "scenario", "config"], cco2),
-    #         forcing = (["timepoints", "scenario", "config"], fsum),
-    #         forcing_co2 = (["timepoints", "scenario", "config"], fco2),
-    #         forcing_aerosol = (["timepoints", "scenario", "config"], faer),
-    #         forcing_nonco2ch4n2oaer = (["timepoints", "scenario", "config"], foth),
-    #     ),
-    #     coords=dict(
-    #         timepoints=np.arange(2015.5, 2101),
-    #         scenario=scenarios,
-    #         config=valid_all,
-    #     ),
-    # )
-    #
-    # ds.to_netcdf(os.path.join(datadir, "output", f"batch_{batch_num:0{4}d}.nc"))
-    # batch_num = batch_num + 1
 
-# # across scenarios
-# pl.plot(ds.forcing[:, :, 0] - ds.forcing_co2[:, :, 0])
-# pl.show()
-#
-# # across configs
-# pl.plot(ds.forcing[:, 0, :] - ds.forcing_co2[:, 0, :])
-# pl.show()
-
-# create a design matrix - non-CO2 forcing is the predictant
+# non-CO2 forcing is the predictant
 # ordering is time, scenario, config
 y = np.ravel(ds.forcing - ds.forcing_co2, order="F")
 
-# to start with, use only CO2 emissions, PD aerosol forcing, and year as the predictors
+# create a design matrix of predictors
+#x = np.zeros((len(y), 47))
 x = np.zeros((len(y), 3))
 
 x[:,0] = np.tile(np.ravel(co2_emissions, order="F"), 1001)
 x[:,1] = np.tile(np.arange(2015, 2101), 10*1001)
+#x[:,2:] = np.repeat(params, 86*10, axis=0)
 x[:,2] = np.repeat(np.ravel(ds.forcing_aerosol.loc[dict(timepoints=2015.5)]), 86)
-
-print(x[:87, :])
 
 # I am not expecting this to do a great job
 from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+
+poly = PolynomialFeatures(degree=2)
+x2 = poly.fit_transform(x)
+print(x2.shape)
+
 reg = linear_model.LinearRegression()
-reg.fit(x, y)
-print(reg.intercept_, reg.coef_)
-print(reg.score(x, y))
+reg.fit(x2, y)
 
-print(reg.predict(np.array([np.linspace(35, 0, 86), np.arange(2015, 2101), np.ones(86)*(-2.0)]).T))
+print(reg.score(x2, y))
 
-pl.plot((ds.forcing - ds.forcing_co2).loc[dict(scenario=ds.scenario[0])])
-pl.show()
+# pred_x = np.concatenate(
+#     [
+#         #np.linspace(35, 0, 86) * np.ones((1, 86)),
+#         x[:86, 0] * np.ones((1, 86)),
+#         np.arange(2015, 2101) * np.ones((1, 86)),
+#         (np.ones((86, 45)) * params.iloc[0:1, ...].values).T
+#     ],
+#     axis=0
+# ).T
+#
+# pl.plot((ds.forcing - ds.forcing_co2).loc[dict(scenario=ds.scenario[0], config=ds.config[0])])
+# pl.plot(reg.predict(pred_x))
+#
+# pred_x = np.concatenate(
+#     [
+#         #np.linspace(35, 0, 86) * np.ones((1, 86)),
+#         x[:86, 0] * np.ones((1, 86)),
+#         np.arange(2015, 2101) * np.ones((1, 86)),
+#         (np.ones((86, 45)) * params.iloc[5:6, ...].values).T
+#     ],
+#     axis=0
+# ).T
+# pl.plot((ds.forcing - ds.forcing_co2).loc[dict(scenario=ds.scenario[0], config=ds.config[5])])
+# pl.plot(reg.predict(pred_x))
+#
+# pl.show()
